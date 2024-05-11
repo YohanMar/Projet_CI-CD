835522e006bf6ecbcd36cafd056f546c180c23d0
Yohan <yohan.martin83@live.com>
2024-05-11 22:49:04 +0200

    Administration terminée

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxStoreModule } from 'ngx-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminConfigComponent } from './admin-config/admin-config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminConfigComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule, // Ajoutez cette ligne
+    AppRoutingModule,
+    NgxStoreModule.forRoot() // Ajoutez cette ligne
   ],
   providers: [
     provideClientHydration()
